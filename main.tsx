c36578b35e3ee28f28437e5c6e8d2510babfec6e
lililiiilllll <thfjgh@naver.com>
2026-01-21 12:08:14 +0900

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // './app'이 아니라 반드시 './App' (대소문자)
-import './index.css';
+import App from './App';
 
 const rootElement = document.getElementById('root');
-
 if (!rootElement) throw new Error('Failed to find the root element');
 
 ReactDOM.createRoot(rootElement).render(
