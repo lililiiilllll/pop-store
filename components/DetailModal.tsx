77a104e901fe139123d25479a8fac9f67edcc540
lililiiilllll <thfjgh@naver.com>
2026-01-28 19:56:52 +0900

    Update DetailModal.tsx

--- components/DetailModal.tsx
+++ components/DetailModal.tsx
@@ -362,6 +362,7 @@ const handleAddReview = async () => {
         resetReviewState();
         onShowSuccess('등록 완료', '후기가 성공적으로 등록되었습니다.');
       }
+    }
     } catch (err) {
       alert('등록 중 오류가 발생했습니다.');
     }
