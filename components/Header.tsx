bff04007f07ba9f405e11c3c0390cc86e2a9c1c0
lililiiilllll <thfjgh@naver.com>
2026-01-29 17:01:59 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,7 +34,6 @@ const Header: React.FC<HeaderProps> = ({
             <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zM12 11.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
           </svg>
         </div>
-        </div>
         <span className="text-[19px] font-black text-[#191f28] tracking-tighter">
           Pin It
         </span>
